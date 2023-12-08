f2e2eb3f7bc15fe61e2e9962bc495d226dcfde93
Kimjeju <overrainbow0315@gmail.com>
2023-12-08 18:08:46 +0900

    댓글 기능 추가 완료

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702025309';
+const cacheName = 'chirpy-1702026499';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
