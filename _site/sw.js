906a90fe96e89e43661620fe54df6e1423b8e743
Kimjeju <overrainbow0315@gmail.com>
2023-12-15 18:16:51 +0900

    프로필 업데이트

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702623686';
+const cacheName = 'chirpy-1702631603';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
