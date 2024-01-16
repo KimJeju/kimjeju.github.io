1d60ba622b25253a70504c6aa1a2f599be7f985e
kimjeju <h.hwi9367@gmail.com>
2024-01-16 23:21:30 +0900

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705414417';
+const cacheName = 'chirpy-1705414830';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
