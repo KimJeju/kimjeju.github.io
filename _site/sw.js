9976a41e5801081033dce60bcaf0c10103f5c557
kimjeju <h.hwi9367@gmail.com>
2024-01-19 15:21:48 +0900

    카테고리 업데이트

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705416298';
+const cacheName = 'chirpy-1705645281';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
