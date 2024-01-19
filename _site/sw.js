d06121a369d24cf8702717a021e53a3ee68ebf12
kimjeju <h.hwi9367@gmail.com>
2024-01-19 15:40:21 +0900

    드림핵 base64 문제풀이

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705645281';
+const cacheName = 'chirpy-1705646395';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
